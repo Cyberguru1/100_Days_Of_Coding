94b5baf93a6a909647a6c30e75567876ca0ae6b1
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-08 03:09:41 -0400

    reverse

--- ALGOS/recursions/reverse.py
+++ ALGOS/recursions/reverse.py
@@ -1,7 +1,14 @@
+##reverse a string with recurrsion time complexity 0(n)
+from typing import Any
+from sympy import re
+
 
 def reverse_func(str) -> None:
-    if len(str) == 1:
+    length = len(str)
+
+    if length == 1:
         return str[0]
-    return str[len(str)-1] + reverse_func(str[0:len(str)-1])
+
+    return str[length-1] + reverse_func(str[0:length-1])
 
 print(reverse_func("hamza"))
