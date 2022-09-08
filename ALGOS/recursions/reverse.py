5082e08bfc27425330c3d04bdb23660e983b9bfd
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-08 07:48:59 -0400

    reverse

--- ALGOS/recursions/reverse.py
+++ ALGOS/recursions/reverse.py
@@ -11,4 +11,10 @@ def reverse_func(str) -> None:
 
     return str[length-1] + reverse_func(str[0:length-1])
 
+def reverse_func2(str) -> None:
+    if str == '':
+        return ''
+    return reverse_func2(str[1:]) + str[0]
+
 print(reverse_func("hamza"))
+print(reverse_func2("gnirtSdesreveR"))
