c6459609a185e57cffef1047d5759ed27d12fea3
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-09 15:17:16 -0400

    palindrome recursion

--- ALGOS/recursions/palindrome.py
+++ ALGOS/recursions/palindrome.py
@@ -1,8 +1,8 @@
 
-def is_palindrome(s:str) -> Any:
+def is_palindrome(s:str) -> any:
     n = len(s)
     if n <= 1:
         return True
     return (s[0] == s[n-1]) and is_palindrome(s[1:n-1])
 
-is_palindrome("madam")
+print(is_palindrome("madam"))
