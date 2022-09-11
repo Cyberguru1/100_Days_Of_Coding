d706f34072046e9bf5df6ebfaae1dcdabc1a34fe
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-11 07:56:46 -0400

    Circullar Dll

--- DataStructure/CircularDLL.py
+++ DataStructure/CircularDLL.py
@@ -1,3 +1,6 @@
+
+
+
 from typing import Any
 
 
@@ -13,11 +16,9 @@ class CircularDLL(object):
         pointer = self.head
         while pointer:
             yield pointer.value
+            if pointer.next == self.head:
+                break
             pointer = pointer.next
-
-    def __str__(self):
-        Clist = [str(vaule) for value in CircularDLL]
-        return " ,".join(clist)
 
     def CreateCDLL(self, value):
         newnode = Node(value)
@@ -26,5 +27,47 @@ class CircularDLL(object):
         self.head = newnode
         self.tail = newnode
 
-    def insert(self, value, pos):
-        pass
+    def insert(self, value, pos = None):
+
+        newnode = Node(value)
+
+        #check if this is the first time of adding data to the list
+        if self.head == self.tail:
+            self.head.next = newnode
+            newnode.next = self.head
+            newnode.prev = self.head
+            self.tail = newnode
+
+        else:
+            if  pos == 0:
+                newnode.next = self.head
+                self.head.prev = newnode
+                self.tail.next = newnode
+                self.head = newnode
+
+            elif pos == 1:
+                self.tail.next = newnode
+                newnode.prev = self.tail
+                newnode.next = self.head
+                self.tail = newnode
+
+            else:
+                counter = 0
+                pointer = self.head
+                while counter < pos -1:
+                    pointer = pointer.next
+
+                nextnode = pointer.next
+                pointer.next = newnode
+                newnode.prev = pointer
+                newnode.next = nextnode
+                nextnode.prev = newnode
+
+CDLL = CircularDLL()
+CDLL.CreateCDLL(0)
+CDLL.insert(1)
+CDLL.insert(2, 0)
+CDLL.insert(3, 0)
+CDLL.insert(4, 0)
+CDLL.insert(5, 0)
+print([value for value in CDLL])
